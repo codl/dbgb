3b0879313c6f97a395fb27dbe0ef03edb9c1d7b5
Corentin Delcourt <codl@codl.fr>
2013-11-26 17:47:33 +0100

    whoaa it's a road holy gripes

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+SRCFILES := $(shell find $(PROJDIRS) -type f -name "*.s")
+
 .PHONY: all run clean mrproper
 
 all: dbgb.gb
@@ -9,13 +11,12 @@ mrproper: clean
 	@-$(RM) $(wildcard dbgb.gb)
 
 
+dbgb.o: $(SRCFILES)
+	wla-gb -o dbgb.s
 
-dbgb.o: dbgb.s Makefile
-	@wla-gb -o dbgb.s
-
-dbgb.gb: dbgb.o link
-	@wlalink link dbgb.gb
+dbgb.gb: dbgb.o linkfile
+	wlalink linkfile dbgb.gb
 
 
 run: dbgb.gb
-	@mednafen dbgb.gb
+	mednafen dbgb.gb >/dev/null &
--- dbgb.s
+++ dbgb.s
@@ -27,28 +27,41 @@ XPos DB
 AbsXPos DB
 DistanceTraveled DW
 Points DB
+
+RoadAngleMod DB
+PerspCounter DB
+PhysXPos DB
 .ENDE
 
 .ORG $40 ; vblank interrupt
+.section "vblank" force
 call vblank
 reti
+.ends
 
 .org $48 ; LCD STAT interrupt
+.section "lstat" force
 call lcdstat
 reti
+.ends
 
 .ORG $100 ; start
+.section "start" force
 nop
 jp start
+.ends
 
-.ORG $0104 ; nintendo logo
+.ORG $104 ; nintendo logo
+.section "nintendo" force
 .DB $CE,$ED,$66,$66,$CC,$0D,$00,$0B,$03,$73,$00,$83,$00,$0C
 .DB $00,$0D,$00,$08,$11,$1F,$88,$89,$00,$0E,$DC,$CC,$6E,$E6
 .DB $DD,$DD,$D9,$99,$BB,$BB,$67,$63,$6E,$0E,$EC,$CC,$DD,$DC
 .DB $99,$9F,$BB,$B9,$33,$3E
-
+.ends
 
 .org $150
+.SECTION "init" semiFREE
+
 start:
     di ; disable interrupts
     ld sp,$FFF4 ; reset stack pointer to $FFF4
@@ -62,7 +75,8 @@ start:
     xor a
     ldh ($40),a
 
-    ld b, 8*2*10
+    ;load tiles
+    ld b, 8*2*$12
     ld de,tiles
     ld hl,$8000
     ldspr:
@@ -74,21 +88,54 @@ start:
 
     ld de,32*32
     ld hl,$9800
-    ld b, $0
     -:
-        inc b
-        ld a, b
-        xor 8
-        jr nz,+
-        xor a; a=0
-        ld b, a
-        +:
-        ld a, b
+        ld a, $0
         ldi (hl),a
         dec de
         ld a,e
         or d
         jr nz,-
+
+    ld c, 0
+    ld hl,$9800 + 32*9
+    -:
+        ld d, 0
+        --:
+            ld a, $8
+            sub c
+            cp d
+            jr z, +
+            jr c, ++
+                ld a, $4 ; dark grey
+                jr +++
+            +:
+                ld a, $a ; roadside left
+                jr +++
+            ++:
+                ld a, 32 - $9
+                add c
+                cp d
+                jr z, +
+                jr c, ++
+                    ld a, $5 ; black
+                    jr +++
+                +:
+                    ld a, $b ; roadside right
+                    jr +++
+                ++:
+                    ld a, $4 ; dark grey
+                    jr +++
+
+            +++:
+            ldi (hl), a
+            inc d
+            ld a, d
+            cp 32
+            jr nz, --
+        inc c
+        ld a, c
+        cp 9
+        jr nz, -
 
 
     ld hl,$fe00
@@ -127,14 +174,11 @@ start:
 
     ei ; enable interrupts again
 
-menu:
-    ;TODO
-    ;jp menu
+    jp main
 
-nameentry:
-    ;TODO
-    ;jp nameentry
+.ends
 
+.section "main" semifree
 
 main:
     halt
@@ -223,14 +267,41 @@ vblank:
             ld a, $80
             sub b
         ++:
-            ld (AbsXPos), a
+        ld (AbsXPos), a
 
 
 
     endvbl:
+        ld a,(AbsXPos)
+        ld b, a
+        srl b
+        srl b
         ld a,(XPos)
-        srl a
+        cp $80
+        ld a, $80 - 80
+        jr nc, +
+            sub b
+            jr ++
+        +:
+            add b
+        ++:
         ldh ($43),a
+        ld (PhysXPos),a
+
+        ld a, (AbsXPos)
+        srl a
+        srl a
+        srl a
+        srl a
+        ld b, a
+        ld a, $8
+        sub b
+        jr nz, +
+        inc a
+        +:
+        ld (RoadAngleMod), a
+        ld (PerspCounter), a
+
         ret
 
 
@@ -242,44 +313,86 @@ lcdstat:
     jr nz, + ; we are at hsync
         ldh a, ($44)
         ; screen is $90 lines high
-        cp $49
+        cp $48
         jr c, +
-        sub $49
-        srl a
-        srl a
-        ld c, a ; c like count, getit
-        cp 0
-        jr z, +
 
-        ld a, (AbsXPos)
-        srl a
-        srl a
-        srl a
-        srl a
-        ld b, a
+        ld a, (PerspCounter)
+        dec a
+        ld (PerspCounter), a
+        jr nz, +
+
+        ld a, (RoadAngleMod)
+        ld (PerspCounter), a
+
         ld a, (XPos)
         cp $80
+        ld a, (PhysXPos)
         jr z, +
         jr c, ++
-        -:
-            add b
-            dec c
-            jr nz, -
+            inc a
             jr +++
         ++:
-        -:
-            sub b
-            dec c
-            jr nz, -
+            dec a
         +++:
-        srl a
         ldh ($43),a
+        ld (PhysXPos), a
 
     +:
     ret
 
+.ends
 
+.section "tools" semifree
+div:
+; divides A by B, result in A
+push de
+push bc
+ld d, 0
+-:
 
+    dec b
+    jr nz,-
+    pop bc
+    push bc
+    inc d
+    dec a
+    jr nz,-
+ld a, d
+pop bc
+pop de
+and a ; set/reset zero flag appropriately, reset carry
+ret
+
+mod:
+; a := a mod b
+push bc
+ld c, a
+ld a, b
+cp 0
+jr z,+
+cp 1
+jr z,+++
+cp 2
+ld a, c
+jr z,++
+-:
+    cp b
+    jr c,+
+    sub b
+    jr z,+
+    jr -
+++:
+    and 1
+    jr +
++++:
+    xor a
++:
+pop bc
+and a
+ret
+.ends
+
+.section "graphics" semifree
 tiles:
 ; 00 test pattern
 .DB %00001111
@@ -317,21 +430,21 @@ tiles:
 .DB %11111111 
 ; 02 white
 .DB %00000000
-.DB %00000000 
 .DB %00000000
-.DB %00000000 
 .DB %00000000
-.DB %00000000 
 .DB %00000000
-.DB %00000000 
 .DB %00000000
-.DB %00000000 
 .DB %00000000
-.DB %00000000 
 .DB %00000000
-.DB %00000000 
 .DB %00000000
-.DB %00000000 
+.DB %00000000
+.DB %00000000
+.DB %00000000
+.DB %00000000
+.DB %00000000
+.DB %00000000
+.DB %00000000
+.DB %00000000
 ; 03 light grey
 .DB %11111111
 .DB %00000000
@@ -351,21 +464,21 @@ tiles:
 .DB %00000000
 ; 04 dark grey
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 .DB %00000000
-.DB %11111111 
+.DB %11111111
 ; 05 black
 .DB %11111111
 .DB %11111111
@@ -451,3 +564,140 @@ tiles:
 .DB %11111111
 .DB %00000000
 .DB %00000000
+; $a roadside left
+.DB %00000001
+.DB %11111111
+.DB %00000011
+.DB %11111111
+.DB %00000111
+.DB %11111111
+.DB %00001111
+.DB %11111111
+.DB %00011111
+.DB %11111111
+.DB %00111111
+.DB %11111111
+.DB %01111111
+.DB %11111111
+.DB %11111111
+.DB %11111111
+; $b roadside right
+.DB %10000000
+.DB %11111111
+.DB %11000000
+.DB %11111111
+.DB %11100000
+.DB %11111111
+.DB %11110000
+.DB %11111111
+.DB %11111000
+.DB %11111111
+.DB %11111100
+.DB %11111111
+.DB %11111110
+.DB %11111111
+.DB %11111111
+.DB %11111111
+; $c roadline 1 left
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+.DB %11000000
+; $d roadline 1 right
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+.DB %00000011
+; $e roadline 2 left
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+.DB %10000000
+; $f roadline 2 right
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+.DB %00000001
+;$10 road line 3 left
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+.DB %11111110
+;$11 road line 3 right
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.DB %01111111
+.ends
